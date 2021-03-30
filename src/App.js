e4fb87540ffe7ade05c6d668bdf6ec20c03efcba
Shivam Kalra <shivamkalra98@gmail.com>
2021-03-30 15:24:59 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  return <div>
-    <h1>Hi World!</h1>
-  </div>;
+    return (
+        <Router>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+        </Router>
+    );
 }
 
 export default App;
