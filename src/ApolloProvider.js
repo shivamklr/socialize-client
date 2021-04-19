7af73b4dd8fb069b143f42c3a37578a7c4504599
Shivam Kalra <shivamkalra98@gmail.com>
2021-04-19 23:51:16 +0530

    add auth context in req headers

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import App from "./App";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-const cache = new InMemoryCache();
+const httpLink = createHttpLink({
+    uri:'http://localhost:5000'
+})
+const authLink = setContext((_,{headers}) => {
+    const token = localStorage.getItem("jsonwebtoken");
+    return {
+        headers: {
+            ...headers,
+            Authorization: token ? `Bearer ${token}` : ``,
+        },
+    };
+});
 const client = new ApolloClient({
-    uri: "http://localhost:5000",
-    cache: cache,
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
 });
 export default (
     <ApolloProvider client={client}>
