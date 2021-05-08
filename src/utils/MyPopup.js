71554e91602ffec147958c2be29d6ea60fe950dd
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-08 18:44:52 +0530

    fix pop up works on hover only

--- src/utils/MyPopup.js
+++ src/utils/MyPopup.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Popup } from "semantic-ui-react";
 
 function MyPopup({ content, children }) {
-    return <Popup inverted content={content} trigger={children} />;
+    return <Popup on='hover' inverted content={content} trigger={children} />;
 }
 
 export default MyPopup;
